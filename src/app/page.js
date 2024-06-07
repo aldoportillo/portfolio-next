1bad94fe2a3e3bd293f8b2aa96c28a3ea86e907c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-07 00:39:27 -0500

    Use CSS modules for home styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Wrapper, Header, Subheader, Highlight, Inline, Bio, AboutMeSection } from "../components/HomeStyles/HomeStyles";
 import Typing from "/public/typing.gif";
 import Image from "next/image";
+import styles from "./Home.module.css"
 
 export const metadata = {
   title: "Home | Aldo Portillo",
@@ -13,18 +13,18 @@ export const metadata = {
 
 function Home({ blogData }) {
   return (
-    <Wrapper>
-      <Header>
-        Hello, I’m <Highlight>Aldo Portillo</Highlight>
-      </Header>
-      <Subheader>Full Stack Engineer | MMA Fighter | Bartender</Subheader>
-      <Inline>
-        <Image src={Typing} alt="Typing" priority unoptimized/>
-        <Bio>
+    <div className={styles.homeWrapper}>
+      <h1 className={styles.heading}>
+        Hello, I’m <span className={styles.highlight}>Aldo Portillo</span>
+      </h1>
+      <h2 className={styles.subheader}>Full Stack Engineer | MMA Fighter | Bartender</h2>
+      <div className={styles.inline}>
+        <Image src={Typing} className={styles.heroImg} alt="Typing" priority unoptimized/>
+        <p className={styles.bio}>
           Based in Chicago. Blending problem-solving skills with creativity and resilience learned from my roles as an MMA fighter and bartender. My diverse experiences fuel my ability to approach technology challenges from unique angles, constantly pushing for growth and innovation.
-        </Bio>
-      </Inline>
-      <AboutMeSection>
+        </p>
+      </div>
+      <div className={styles.aboutMe}>
         <h2>About Me</h2>
         <p>Hello! I&apos;m <strong>Aldo Portillo</strong>, a passionate <strong>Full Stack Engineer</strong> based in Chicago. With a foundational background in organic chemistry, I bring a methodical and analytical approach to developing robust, scalable web applications.</p>
         
@@ -33,9 +33,9 @@ function Home({ blogData }) {
         <p>My journey is driven by honor, respect, and a commitment to positively impact society. I eagerly embrace life&apos;s adventures and challenges, especially in software, mixology, martial arts, and personal development. Tutoring in software and math allows me to share my knowledge and help others grow in their skills.</p>
         
         <p>I&apos;m excited to connect and share this journey with you. Whether you&apos;re interested in collaborative projects, or just wish to chat about technology, climbing, or anything else, feel free to <Link href="/contact">contact me</Link> or explore my <Link href="/projects">projects</Link>. Let’s navigate the adventures of life together!</p>
-      </AboutMeSection>
+      </div>
       {/* <Posts blogData={blogData} /> */}
-    </Wrapper>
+    </div>
   );
 }
 
