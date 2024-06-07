63efc5972e7adc54bf4e5e59df95d5e0138b80a3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-07 15:41:54 -0500

    Adjust UI for demo

--- src/components/LoadBalancerDemo/LoadBalancerDemo.js
+++ src/components/LoadBalancerDemo/LoadBalancerDemo.js
@@ -1,9 +1,9 @@
 "use client";
-import React, { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
-import { FaServer } from 'react-icons/fa';
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { FaServer } from "react-icons/fa";
 import { GiServerRack } from "react-icons/gi";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 function LoadBalancerDemo() {
   const [strategy, setStrategy] = useState("weightRoundRobin");
@@ -15,11 +15,11 @@ function LoadBalancerDemo() {
   const [servers, setServers] = useState([
     { id: 0, name: "Server 0", weight: 0.5, hits: 0 },
     { id: 1, name: "Server 1", weight: 0.3, hits: 0 },
-    { id: 2, name: "Server 2", weight: 0.2, hits: 0 }
+    { id: 2, name: "Server 2", weight: 0.2, hits: 0 },
   ]);
 
   const updateWeight = (id, newWeight) => {
-    const updatedServers = servers.map(server => {
+    const updatedServers = servers.map((server) => {
       if (server.id === id) {
         return { ...server, weight: newWeight };
       }
@@ -37,14 +37,14 @@ function LoadBalancerDemo() {
       let weightSum = 0;
 
       for (let i = 0; i < servers.length; i++) {
-        weightSum += servers[i].weight / totalWeight * requestsPerCycle;
+        weightSum += (servers[i].weight / totalWeight) * requestsPerCycle;
         if (currentCount < weightSum) {
           requestCounter.current++;
           return i;
         }
       }
     } else if (strategy === "sourceIpHash") {
-      return requestNumber % servers.length; 
+      return requestNumber % servers.length;
     }
     return null;
   };
@@ -52,7 +52,6 @@ function LoadBalancerDemo() {
   const sendRequest = () => {
     const serverIndex = getNextServerIndex();
     if (serverIndex !== null) {
-
       const updatedServers = servers.map((server, index) => {
         if (index === serverIndex) {
           return { ...server, hits: server.hits + 1 };
@@ -60,21 +59,27 @@ function LoadBalancerDemo() {
         return server;
       });
       setServers(updatedServers);
-  
 
       const newRequest = {
         id: requestNumber,
-        serverId: serverIndex
+        serverId: serverIndex,
       };
-      setActiveRequests(current => [...current, newRequest]);
-  
+      setActiveRequests((current) => [...current, newRequest]);
+
       setTimeout(() => {
-        setActiveRequests(current => current.filter(req => req.id !== newRequest.id));
+        setActiveRequests((current) =>
+          current.filter((req) => req.id !== newRequest.id)
+        );
       }, 1000);
-  
-      console.log("Request sent to server", servers[serverIndex].name, "Hits:", servers[serverIndex].hits);
+
+      console.log(
+        "Request sent to server",
+        servers[serverIndex].name,
+        "Hits:",
+        servers[serverIndex].hits
+      );
     }
-    setRequestNumber(prev => prev + 1);
+    setRequestNumber((prev) => prev + 1);
   };
 
   useEffect(() => {
@@ -84,68 +89,122 @@ function LoadBalancerDemo() {
     setServers([
       { id: 0, name: "Server 0", weight: 0.5, hits: 0 },
       { id: 1, name: "Server 1", weight: 0.3, hits: 0 },
-      { id: 2, name: "Server 2", weight: 0.2, hits: 0 }
+      { id: 2, name: "Server 2", weight: 0.2, hits: 0 },
     ]);
   }, [strategy]);
-  
-  return (
-    <LoadBalancerAnimationWrapper>
-      <BalancerAndServers>
-        <Icon onClick={sendRequest}>
-          <GiServerRack />
-          <p>Load Balancer</p>
-        </Icon>
-        <Servers>
-          {servers.map(server => (
-            <Icon key={server.id}>
-              <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <FaServer />
-                <Text>server-{server.id}</Text>
-                <Text>{strategy === "weightRoundRobin" && <Input><label>Weight: </label><input type="number" value={server.weight} onChange={e => updateWeight(server.id, parseFloat(e.target.value))} min="0" step="0.1" /></Input>}</Text>
-                <Text>Hits: {server.hits}</Text>
-              </motion.div>
-              {activeRequests.map(req => req.serverId === server.id && (
-                <Request
-                  key={req.id}
-                  initial={{ x: -100, opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  exit={{ x: 100, opacity: 0 }}
-                  transition={{ duration: 1 }}
-                >
-                  Request: {req.id}
-                </Request>
-              ))}
-            </Icon>
-          ))}
-        </Servers>
-      </BalancerAndServers>
-      <Options>
-        <Button onClick={() => setStrategy("roundRobin")} selected={strategy === "roundRobin"}>Round Robin</Button>
-        <Button onClick={() => setStrategy("weightRoundRobin")} selected={strategy === "weightRoundRobin"}>Weighted Round Robin</Button>
-        <Button onClick={() => setStrategy("sourceIpHash")} selected={strategy=== "sourceIpHash"}>Source IP Hash</Button>
-        <div>
-        {strategy === "weightRoundRobin" && (
-          <Input>
-            <label>RPS:</label>
-            <input type="number" value={requestsPerCycle} onChange={e => setRequestsPerCycle(parseInt(e.target.value, 10))} min="1" />
-          </Input>
-        )}
-        </div>
-      </Options>
-    </LoadBalancerAnimationWrapper>
-  );
-  
-}
 
+  return (
+    <LoadBalancerDemoWrapper>
+      <Options>
+        <Button
+          onClick={() => setStrategy("roundRobin")}
+          selected={strategy === "roundRobin"}
+        >
+          Round Robin
+        </Button>
+        <Button
+          onClick={() => setStrategy("weightRoundRobin")}
+          selected={strategy === "weightRoundRobin"}
+        >
+          Weighted Round Robin
+        </Button>
+        <Button
+          onClick={() => setStrategy("sourceIpHash")}
+          selected={strategy === "sourceIpHash"}
+        >
+          Source IP Hash
+        </Button>
+      </Options>
+      <LoadBalancerAnimationWrapper>
+        <BalancerAndServers>
+          <Icon onClick={sendRequest}>
+            <GiServerRack />
+            <p>Load Balancer</p>
+            <div>
+              {strategy === "weightRoundRobin" && (
+                <Input>
+                  <label>RPS:</label>
+                  <input
+                    type="number"
+                    value={requestsPerCycle}
+                    onChange={(e) =>
+                      setRequestsPerCycle(parseInt(e.target.value, 10))
+                    }
+                    min="1"
+                  />
+                </Input>
+              )}
+            </div>
+          </Icon>
+          <Servers>
+            {servers.map((server) => (
+              <Icon key={server.id}>
+                <motion.div
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <FaServer />
+                  <Text>server-{server.id}</Text>
+                  <Text>
+                    {strategy === "weightRoundRobin" && (
+                      <Input>
+                        <label>Weight: </label>
+                        <input
+                          type="number"
+                          value={server.weight}
+                          onChange={(e) =>
+                            updateWeight(server.id, parseFloat(e.target.value))
+                          }
+                          min="0"
+                          step="0.1"
+                        />
+                      </Input>
+                    )}
+                  </Text>
+                  <Text>Hits: {server.hits}</Text>
+                </motion.div>
+                {activeRequests.map(
+                  (req) =>
+                    req.serverId === server.id && (
+                      <Request
+                        key={req.id}
+                        initial={{ x: -100, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        exit={{ x: 100, opacity: 0 }}
+                        transition={{ duration: 1 }}
+                      >
+                        Request: {req.id}
+                      </Request>
+                    )
+                )}
+              </Icon>
+            ))}
+          </Servers>
+        </BalancerAndServers>
+      </LoadBalancerAnimationWrapper>
+    </LoadBalancerDemoWrapper>
+  );
+}
 
 export default LoadBalancerDemo;
 
-const Button = styled.button`
+const LoadBalancerDemoWrapper = styled.div`
   background-color: #333;
+  border-radius: 10px;
+  padding: 0px 20px 10px 20px;
+}`;
+
+const Request = styled(motion.div)`
+  background-color: #5eddac;
+  color: #242424;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin-top: 5px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.selected ? "#4CAF50" : "#333")};
   color: white;
   border: none;
   padding: 10px 20px;
@@ -153,18 +212,15 @@ const Button = styled.button`
   cursor: pointer;
   outline: none;
   transition: background-color 0.2s;
+  width: 100%;
 
   &:hover {
-    background-color: "green";
-  }
-
-  &[selected="true"] {
-    background-color: "blue";
+    background-color: ${(props) => (props.selected ? "#66bb6a" : "#555")};
   }
 `;
 
 const LoadBalancerAnimationWrapper = styled.div`
-  background-color: #333;
+  background-color: #e6e8f0;
   color: white;
   display: flex;
   flex-direction: column;
@@ -192,6 +248,7 @@ const Icon = styled.div`
   justify-content: center;
   font-size: 1em;
   margin: 10px;
+  color: #333;
 
   svg {
     font-size: 2em;
@@ -201,14 +258,10 @@ const Icon = styled.div`
 
 const Options = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 20px;
+  justify-content: space-around;
   margin-top: 20px;
-`;
-
-const Request = styled(motion.div)`
-  color: white;
-  background-color: green;
+  padding: 10px 0;
+  width: 100%;
 `;
 
 const Text = styled.p`
@@ -216,7 +269,6 @@ const Text = styled.p`
 `;
 
 const Input = styled.span`
-
   display: flex;
   align-items: flex-end;
   justify-content: center;
@@ -225,14 +277,14 @@ const Input = styled.span`
 
   input {
     font-size: 0.6em;
-    width:40px;
+    width: 40px;
     margin-top: 5px;
     border: none;
     outline: none;
-    border-bottom: 1px solid #ccc
+    border-bottom: 1px solid #ccc;
   }
 
   input:focus {
     outline: none;
   }
-  `
+`;
