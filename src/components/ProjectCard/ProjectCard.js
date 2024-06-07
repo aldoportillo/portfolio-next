773e464bcd1b2a9722c84457aeea821876d18f9e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-07 01:50:10 -0500

    Use CSS modules for project card styles

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,49 +1,16 @@
-"use client";
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import Image from "next/image";
+import styles from "./ProjectCard.module.css";
 
 function ProjectCard({ project }) {
   return (
-    <Card>
-      <ProjectImage src={project.image} alt={project.name} />
-      <ProjectInfo>
-        <ProjectTitle>{project.name}</ProjectTitle>
-        <ProjectDescription>{project.description}</ProjectDescription>
-      </ProjectInfo>
-    </Card>
+    <div className={styles.projectCard}>
+      <Image src={project.image} alt={project.name}/>
+      <div className={styles.projectInfo}>
+        <h2 className={styles.projectTitle}>{project.name}</h2>
+        <p className={styles.projectDescription}>{project.description}</p>
+      </div>
+    </div>
   );
 }
-
-const Card = styled.div`
-  background: #333;
-  color: #fff;
-  border-radius: 8px;
-  overflow: hidden;
-`;
-
-const ProjectImage = styled.img`
-  width: 100%;
-  height: auto;
-  display: block;
-`;
-
-const ProjectInfo = styled.div`
-  padding: 15px;
-`;
-
-const ProjectTitle = styled.h2`
-  font-size: 1.5em;
-  color: #5eddac;
-`;
-
-const ProjectDescription = styled.p`
-  font-size: 1em;
-  color: #ccc;
-`;
-
-ProjectCard.propTypes = {
-    project: PropTypes.object.isRequired
-};
-
 
 export default ProjectCard;
